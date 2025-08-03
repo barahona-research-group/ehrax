80b86d8ebba93f755a324bafc36bc327e80a6c4b
A-Alaa <asem_alla@msn.com>
2025-08-03 01:17:50 +0100

    refine logging.

--- ehrax/example_schemes/mixed_icd.py
+++ ehrax/example_schemes/mixed_icd.py
@@ -124,9 +124,9 @@ class MixedICDScheme(CodingScheme):
                 f"({self.name})) and the standard ({icd_schemes[standard_version].name}). ",
                 lost_codes_df, f'mixed_to_{icd_schemes[standard_version].name}_lost_codes'))
 
-        lost_stats = len(dataframe) - stats.iloc[:, 1:].rename(columns=lambda c: f'Lost {c}')
+        lost_stats = dataframe['count'] - stats.iloc[:, 1:].rename(columns=lambda c: f'Lost {c}')
         stats = pd.concat([stats, lost_stats], axis=1)
-        norm_stats = stats.rename(index=lambda i: f'%{i}') / len(dataframe)
+        norm_stats = stats.rename(index=lambda i: f'%{i}') / dataframe['count']
         stats = pd.concat([stats, norm_stats], axis=0)
         dataframe_logger.info((
             f"Statistics of the mapping between the Mixed ICD ({self.name}) and the standard ICD schemes.",
