839845edbeb6845d2ad4841113c01550f9c658ae
Asem <asem.a.abdelaziz@proton.me>
2025-08-14 12:00:12 +0100

    Dev (#12)
    - Substitute missing gender/ethnicity while loading data tables.
    - Improve error messages.
    - SNOMED: develop and enrich with SNOMED-CT UK extension processors.

--- ehrax/base.py
+++ ehrax/base.py
@@ -42,7 +42,12 @@ class _ModuleMeta(type(eqx.Module)):
             return f"{cls.__module__}.{cls.__qualname__}"
 
         def __get_factory__(type_str: str) -> type[Self]:
-            return _factory_registry[type_str]
+            try:
+                return _factory_registry[type_str]
+            except KeyError:
+                raise KeyError(f"{type_str} is not a valid factory type. "
+                               f"You may need to import a module that was used during the construction of "
+                               f"{type_str} object(s).")
 
         cls.__class_key__ = classmethod(__class_key__)
         cls.__get_factory__ = __get_factory__
@@ -452,15 +457,7 @@ MAX_SEGMENT_SIZE = 500
 
 
 class AbstractVxData(AbstractHDFSerializable):
-    """
-    AbstractVxData class represents vectorized data object, which inherits from eqx.AbstractVxData.
-
-    Methods:
-
-        to_cpu() - Copy arrays in module to CPU.
-
-        to_device() - Copy arrays in module to device.
-    """
+    # TODO: doc: all subclasses of AbstractVxData must be initable from its attributes.
 
     def __check_init__(self):
         for f in (k for k in self.fields):
